fd210d717093cf2efaed15ffb72c7cb6c22b5fee
Leonel Araújo <leonel.araujo27@gmail.com>
2015-10-19 21:29:39 +0100

    novos tweets

--- tweets.js
+++ tweets.js
@@ -2,7 +2,6 @@ module.exports = {
 	text : [
 		'se ganharmos digo easy',
 		'só dão dodge quando me calha xerath',
-		'siga aram',
 		'convida a neeyd',
 		'não vou po bot com o random',
 		'não jogo solocu',
@@ -30,6 +29,11 @@ module.exports = {
 		'viktor é memo fixe a support',
 		'fds deixem-me dar lifesteal',
 		'*está 2-12-2* é que eu até tou a jogar bem',
-		'não consigo dar ultimate, tá bugado'
+		'não consigo dar ultimate, tá bugado',
+		'e acertou-me a merda da seta',
+		'esta riot só tem merda na cabeça, só pode',
+		'eheh tostadeira eheh jogar no gameboy eheh',
+		'joguem este sem mim, vou lavar os dentes',
+		'perem aí que vou cagar'
 	]
 }
